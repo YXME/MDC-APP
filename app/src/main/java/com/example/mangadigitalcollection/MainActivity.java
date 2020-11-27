7124c764b49fac55f6731b965ddca77065f39b2d
EmericJdt <39245788+EmericJdt@users.noreply.github.com>
2020-11-27 21:09:36 +0100

    - Fixed warning on project

--- app/src/main/java/com/example/mangadigitalcollection/MainActivity.java
+++ app/src/main/java/com/example/mangadigitalcollection/MainActivity.java
@@ -27,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.hide();
-        token = getIntent().getStringExtra("token");
-        final TextView helloTextView = (TextView) findViewById(R.id.textView);
-        if (listProducts != null)
-            helloTextView.setText(listProducts.toString());
+
+
     }
 }
