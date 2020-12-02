f42b265834d8a07ac6ef667197699a0fef736391
EmericJdt <39245788+EmericJdt@users.noreply.github.com>
2020-12-02 20:34:08 +0100

    Corrections de certains noms de fichiers.

--- app/src/main/java/com/example/mangadigitalcollection/ProfilActivity.java
+++ app/src/main/java/com/example/mangadigitalcollection/ProfilActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class random extends AppCompatActivity {
+public class ProfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random);
+        setContentView(R.layout.activity_profil1);
     }
 }
